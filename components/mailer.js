655a5b45a0b2bc00649142957bc35f620dc7fcd0
Achtrick <achrefmtir7@gmail.com>
2022-02-28 17:00:01 +0100

    prod v5

--- components/mailer.js
+++ components/mailer.js
@@ -1,29 +1,13 @@
 const nodemailer = require("nodemailer");
+require("dotenv").config();
 
 exports.transporter = nodemailer.createTransport({
-  service: "SMTP",
+  service: "IMAP",
+  host: "SSL0.OVH.NET",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.AUTH_EMAIL,
     pass: process.env.AUTH_PASS,
   },
 });
-
-exports.mailcss = {
-  background: `
-  style="
-  background: linear-gradient(90deg, rgba(171,74,230,1) 0%, rgba(160,20,103,1) 49%);
-  border-radius: 5px;
-  padding-left: 10px;
-  padding-right: 10px;
-  padding-top: 5px;
-  padding-bottom: 5px;
-  color: white;"`,
-  body: `
-  style="background: white;
-  border-radius: 5px;
-  padding-left: 10px;
-  padding-right: 10px;
-  padding-top: 5px;
-  padding-bottom: 5px;
-  color: black;"`,
-};
--- index.js
+++ index.js
@@ -13,6 +13,23 @@ const { client } = require("./database/models/client.model");
 
 // server settings
 const port = process.env.PORT || 3001;
+// production samesite = none // secure = true ..
+// development samesite = lax // secure = false ..
+const cookie =
+  process.env.NODE_ENV === "production"
+    ? {
+        httpOnly: true,
+        secure: true,
+        maxAge: 1000 * 60 * 60 * 48,
+        sameSite: "none",
+      }
+    : {
+        httpOnly: true,
+        secure: false,
+        maxAge: 1000 * 60 * 60 * 48,
+        sameSite: "lax",
+      };
+
 app = express();
 app.enable("trust proxy");
 app.listen(port, () => {
@@ -29,8 +46,7 @@ app.use(
     credentials: true,
   })
 );
-// production samesite = none // secure = true ..
-// development samesite = lax // secure = false ..
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
@@ -40,12 +56,7 @@ app.use(
     resave: false,
     secret: "AnytimeAnywhereMailer",
     saveUninitialized: true,
-    cookie: {
-      httpOnly: true,
-      secure: false,
-      maxAge: 1000 * 60 * 60 * 48,
-      sameSite: "lax",
-    },
+    cookie: cookie,
   })
 );
 
@@ -68,7 +79,6 @@ app.post("/createAccount", (req, res) => {
   });
   try {
     newuser.save();
-
     res.send("SUCCESS");
   } catch (err) {
     res.send("ERROR");
@@ -142,21 +152,6 @@ app.post("/addclient", (req, res) => {
   }
 });
 
-app.get("/sendmails", async (req, res) => {
-  const clientsList = await client.find({});
-  var emails = [];
-  clientsList.map((client) => {
-    emails.push(client.email);
-  });
-  mailer.transporter.sendMail({
-    from: '"Anytime & Anywhere" <' + process.env.AUTH_EMAIL + ">",
-    to: email,
-    subject: "Activate account.",
-    text: "Follow the instructions to activate your account.",
-    html: "",
-  });
-});
-
 app.post("/getclients", async (req, res) => {
   var currentPage;
   var searchTerm;
@@ -187,7 +182,7 @@ app.post("/getclients", async (req, res) => {
       })
       .limit(10)
       .skip((currentPage - 1) * 10)
-      .sort({ date: -1 })
+      .sort({ createdAt: -1 })
       .exec();
 
     const count = await client.countDocuments({
@@ -214,4 +209,253 @@ app.post("/getclient", (req, res) => {
       res.send(row);
     }
   });
+});
+
+app.post("/sendmails", async (req, res) => {
+  const subject = req.body.subject,
+    message = req.body.message;
+  const clientsList = await client.find({});
+
+  if (clientsList.length >= 1) {
+    clientsList.forEach((client) => {
+      mailer.transporter.sendMail(
+        {
+          from: '"Anytime & Anywhere" <' + process.env.AUTH_EMAIL + ">",
+          to: client.email,
+          subject: subject,
+          html: `<html>
+                  <head>
+                    <link rel="preconnect" href="https://fonts.googleapis.com" />
+                    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+                    <link
+                      href="https://fonts.googleapis.com/css2?family=Poppins:wght@200&family=Source+Sans+Pro:wght@300;400&display=swap"
+                      rel="stylesheet"
+                    />
+                  </head>
+                  <body>
+                    <div
+                      style="
+                        width: 100%;
+                        background-color: #ccc;
+                        display: inline-flex;
+                        margin: 0px;
+                        padding-top: 100px;
+                        padding-bottom: 100px;
+                      "
+                    >
+                      <div style="width: 10%; background-color: #ccc"></div>
+                      <div style="width: 80%; background-color: #fff">
+                        <div
+                          style="
+                            width: 100%;
+                            display: inline-flex;
+                            background-color: rgb(255, 255, 255);
+                          "
+                        >
+                          <div style="width: 20%"></div>
+                          <div style="width: 60%">
+                            <div
+                              style="
+                                display: flex;
+                                width: 100%;
+                                justify-content: center;
+                                padding-top: 50px;
+                              "
+                            >
+                              <img
+                                width="100%"
+                                alt="header"
+                                src="https://lh3.google.com/u/0/d/16yDuGG3_Xm3MbaG-ipgcvv5aRI96513N=w1366-h568-iv1"
+                              />
+                            </div>
+                            <div
+                              style="
+                                display: block;
+                                width: 100%;
+                                justify-content: center;
+                                padding-top: 50px;
+                                padding-bottom: 100px;
+                                font-family: 'Poppins', sans-serif;
+                                font-family: 'Source Sans Pro', sans-serif;
+                              "
+                            >
+                              <div
+                                style="
+                                  width: 100%;
+                                  display: inline-block;
+                                  justify-content: center;
+                                  font-size: x-large;
+                                  text-align: center;
+                                "
+                              >
+                                Trouvez vos prochains clients grâce
+                                <br />
+                                <div style="display: inline-flex">
+                                  <div style="padding-right: 5px">à</div>
+                                  <div style="padding-right: 5px">un</div>
+                                  <div style="padding-right: 5px">ou</div>
+                                  <div style="padding-right: 5px">une</div>
+                                  <div style="font-weight: bold; padding-left: 5px">
+                                    spécialiste marketing
+                                  </div>
+                                </div>
+                                <br />
+                                <br />
+                                <div style="font-size: large; text-align: justify">
+                                  Bonjour:${client.companyName}
+                                </div>
+                                <br />
+                                <div style="font-size: large; text-align: justify">
+                                ${message}
+                                </div>
+                                <div style="padding-top: 100px">
+                                  <a href="tel:+21654563326"
+                                    ><img
+                                      width="100%"
+                                      alt="callBtn"
+                                      src="https://lh3.google.com/u/0/d/1sBiO0k_R5HExQ3SCHCvnZZuVlpfVY1EB=w1366-h625-iv1"
+                                  /></a>
+                                </div>
+                                <br />
+                                <div style="font-size: large; text-align: left">
+                                  Bien cordialement,
+                                </div>
+                                <br />
+                                <br />
+                                <div
+                                  style="
+                                    display: inline-flex;
+                                    justify-content: center;
+                                    align-items: center;
+                                    width: 100%;
+                                  "
+                                >
+                                  <div style="width: 10%"></div>
+                                  <div style="width: 20%">
+                                    <img
+                                      width="100%"
+                                      alt="nizarPhoto"
+                                      src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6e/Breezeicons-actions-22-im-user.svg/1200px-Breezeicons-actions-22-im-user.svg.png"
+                                      style="border-radius: 50%; padding: 10px; height: auto"
+                                    />
+                                  </div>
+                                  <div style="width: 60%; margin-left: 20px">
+                                    <div
+                                      style="
+                                        font-size: large;
+                                        text-align: left;
+                                        font-weight: bold;
+                                      "
+                                    >
+                                      Nizar JERFEL
+                                    </div>
+                                    <div
+                                      style="
+                                        font-size: large;
+                                        text-align: left;
+                                        color: rgb(102, 102, 102);
+                                      "
+                                    >
+                                      Votre spécialiste marketing digital
+                                    </div>
+                                  </div>
+                                  <div style="display: block; width: 10%"></div>
+                                </div>
+                                <br />
+                                <br />
+                                <div
+                                  style="
+                                    display: inline-flex;
+                                    justify-content: center;
+                                    align-items: center;
+                                    width: 100%;
+                                  "
+                                >
+                                  <div style="width: 30%"></div>
+                                  <div style="width: 60%">
+                                    <div
+                                      style="
+                                        display: inline-flex;
+                                        justify-content: center;
+                                        align-items: center;
+                                        width: 100%;
+                                      "
+                                    >
+                                      <a
+                                        href="https://www.facebook.com/anywhere4"
+                                        style="width: calc(100% / 3)"
+                                      >
+                                        <img
+                                          width="100%"
+                                          style="padding: 10px"
+                                          src="https://lh3.google.com/u/0/d/1p-_hJBtvmkLCKSJyeMZ0fc8ctlzPoX8C=w1366-h625-iv1"
+                                          alt="Facebook"
+                                        />
+                                      </a>
+                                      <a
+                                        href="https://www.instagram.com/anytime4anywhere/"
+                                        style="width: calc(100% / 3)"
+                                      >
+                                        <img
+                                          width="100%"
+                                          style="padding: 10px"
+                                          src="https://lh3.google.com/u/0/d/1IDKWCMD05SDTKPqUj4NnidBKSuSkNkFO=w860-h625-iv1"
+                                          alt="Instagram"
+                                        />
+                                      </a>
+                                      <a
+                                        href="https://www.linkedin.com/company/anytime-anywhere/"
+                                        style="width: calc(100% / 3)"
+                                      >
+                                        <img
+                                          width="100%"
+                                          style="padding: 10px"
+                                          src="https://lh3.google.com/u/0/d/1kFCu6Z-0IoEqrLtt1jFM37jDVPw8Q68r=w1366-h625-iv1"
+                                          alt="LinkedIn"
+                                        />
+                                      </a>
+                                    </div>
+                                  </div>
+                                  <div style="width: 30%"></div>
+                                </div>
+                                <br />
+                                <div
+                                  style="
+                                    padding-top: 20px;
+                                    display: block;
+                                    text-align: center;
+                                    width: 100%;
+                                    font-size: 20px;
+                                  "
+                                >
+                                  Anytime & Anywhere
+                                  <br />
+                                  <br />
+                                  <br />
+                                  <hr />
+                                  www.anytime4anywhere.com
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      <div style="width: 10%; background-color: #ccc"></div>
+                    </div>
+                  </body>
+                </html>
+                `,
+        },
+        (err) => {
+          if (err) {
+            res.send("ERROR");
+          } else {
+            res.send("SUCCESS");
+          }
+        }
+      );
+    });
+  } else {
+    res.send("EMPTY");
+  }
 });
